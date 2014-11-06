586140df3c1749911a5fb3fda6c9d8752c0d990f
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-11-06 10:28:48 -0300

    Bump version 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures cespi_mysql'
 long_description 'Installs/Configures cespi_mysql'
-version          '0.1.1'
+version          '0.1.2'
 
 depends 'mysql-multi', '~> 1.4.1'
 depends 'mysql',       '~> 5.6.0'
