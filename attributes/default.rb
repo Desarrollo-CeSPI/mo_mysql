9580cea1751187a329043cb720e52df27550da2b
Christian Rodriguez <car@cespi.unlp.edu.ar>
2014-12-06 01:13:15 -0300

    can parameterize slow_query

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['mo_mysql']['cluster_name'] = 'cluster_name'
 default['mo_mysql']['master'] = 'localhost'
 default['mo_mysql']['slaves'] = []
 default['mo_mysql']['server_repl_password'] = 'repl_mo_mysql_pass'
+default['mo_mysql']['slow_query_log'] = 0
