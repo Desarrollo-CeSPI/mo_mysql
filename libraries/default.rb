1e1113b9b51984e8f8df33ce3dfcb36ac7746bb2
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-04-06 01:17:47 -0300

    Add db type to mysql

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,6 +1,7 @@
 def mo_mysql_data_for_cluster(cluster_name)
   data_bag_item(node['mo_mysql']['databag'], cluster_name).tap do |data|
     Chef::Log.info("Loaded data for Mysql cluster #{node['mo_mysql']['databag']}/#{cluster_name}")
+    data['type'] = 'mysql'
   end
 end
 
