f7b5d52e964aca181639e11682720bc01ec9500c
Christian Rodriguez <car@cespi.unlp.edu.ar>
2015-08-17 08:20:20 -0300

    Fix postrotate script

--- recipes/logrotate.rb
+++ recipes/logrotate.rb
@@ -1,9 +1,10 @@
 if node['mysql_tuning'] &&
     node['mysql_tuning']['logging.cnf'] &&
-    node['mysql_tuning']['logging.cnf']['slow_query_log'] == 'ON' &&
-    node['mysql_tuning']['logging.cnf']['slow_query_log_file']
+    node['mysql_tuning']['logging.cnf']["mysqld"] &&
+    node['mysql_tuning']['logging.cnf']["mysqld"]['slow_query_log'] == 'ON' &&
+    node['mysql_tuning']['logging.cnf']["mysqld"]['slow_query_log_file']
 
-  relative = node['mysql_tuning']['logging.cnf']['slow_query_log_file']
+  relative = node['mysql_tuning']['logging.cnf']["mysqld"]['slow_query_log_file']
 
   logrotate_app "mo-mysql-slowlog" do
     path      "/var/lib/mysql-#{node['mysql-multi']['service_name']}/#{relative}"
@@ -12,7 +13,7 @@ if node['mysql_tuning'] &&
     minsize   '1M'
     rotate    52
     create    %W(660 mysql mysql).join ' '
-    postrotate "    mysql -e 'select @@global.long_query_time into @lqt_save; set global long_query_time=2000; select sleep(2); FLUSH LOGS; select sleep(2); set global long_query_time=@lqt_save;"
+    postrotate "    mysql -e 'select @@global.long_query_time into @lqt_save; set global long_query_time=2000; select sleep(2); FLUSH LOGS; select sleep(2); set global long_query_time=@lqt_save;'"
   end
 
 end
